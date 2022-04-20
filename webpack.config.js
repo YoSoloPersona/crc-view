e66ce0ca0856d4d8ef8d034e59166b654574d89e
YoSoloPersona <79009569+YoSoloPersona@users.noreply.github.com>
2022-04-20 22:40:06 +0300

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV == 'production';
+const watch = process.env.NODE_ENV == 'development';
+
+const stylesHandler = mode ? MiniCssExtractPlugin.loader : 'style-loader';
+
+const main = {
+    mode,
+    target: 'electron-main',
+    watch,
+    entry: './src/main.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/']
+            }
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ]
+    }
+};
+
+const render = {
+    mode,
+    target: 'electron-renderer',
+    watch,
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        })
+
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/']
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler, 'css-loader']
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset'
+            }
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ]
+    }
+};
+
+module.exports = [main, render];
